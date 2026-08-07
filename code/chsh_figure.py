3fc3a633b01d7fa386231ca780c3191f09d57298
rtcon <166172292+rtcon@users.noreply.github.com>
2026-08-07 08:02:45 -0700

    adding mit license to scripts, adding detail about massless interpretation

--- code/chsh_figure.py
+++ code/chsh_figure.py
@@ -1,3 +1,37 @@
+# ---------------------------------------------------------------------
+# chsh_figure.py
+# Figure generation for "CHSH by Counting: Bell Violation in a Finite
+# Configuration Ontology" (Zenodo, doi:10.5281/zenodo.21523901).
+#
+# Part of the Configuration Realism series:
+# "Configuration Realism: Foundations" (doi:10.5281/zenodo.21523984).
+#
+# Author:    Russell Tillitt, Independent Researcher, San Francisco, CA
+# Copyright: (c) 2026 Russell Tillitt
+# License:   MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# If you use this code, please cite the paper above.
+# ---------------------------------------------------------------------
 """Generate chsh_results.png: three-panel figure for 'CHSH by Counting'.
 
 Panel 1: counted correlator E vs analyzer separation, coherent class vs
